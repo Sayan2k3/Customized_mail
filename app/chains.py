0f7b655db144bc1c29cfd40fa2c6af665f45c835
Sayan Paul <121636215+Sayan2k3@users.noreply.github.com>
2025-04-15 01:49:24 +0530

    Initial commit

---
+++ app/chains.py
@@ -0,0 +1,59 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            I am Sayan Paul, a Computer Science and Business Systems student at the Institute of Engineering and Management (IEM). With a strong foundation in AI and software development, I specialize in creating solutions that streamline business operations and drive efficiency.​
+
+            In my academic and project work, I've developed AI tools that automate routine tasks, optimize workflows, and provide actionable insights. For instance, I recently led a project where we designed an AI-powered email generator that increased outreach efficiency by 40%.​
+
+            I am confident that my skills in AI and software development can be leveraged to address your business needs, leading to improved scalability and reduced operational costs.​
+
+            I would welcome the opportunity to discuss how I can contribute to your organization's success. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
